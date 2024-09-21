4a1bf91996dfc617a68a5b4ca17fd595e1b564f0
Darak376 <130400141+Darak376@users.noreply.github.com>
2024-09-21 14:32:50 -0600

    Update App.js for career page navigation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from "react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./Homepage";
-import "./App.css";
-import Services from "./components/Services";
 import About from "./components/About";
+import Services from "./components/Services";
+import Carousel from "./components/Carousel";
 import Contact from "./components/Contact";
 import Contact1 from "./components/Contact1";
 import Footer from "./components/Footer";
-import Carousel from "./components/Carousel";
+import Careers from "./forms/Careers";
+import "./App.css";
 
 function App() {
   return (
+    <Router>
+      <Routes>
+        {/* Main scrollable page */}
+        <Route path="/" element={<MainPage />} />
+
+        {/* Separate Careers page */}
+        <Route path="/careers" element={<Careers />} />
+      </Routes>
+    </Router>
+  );
+}
+
+function MainPage() {
+  return (
     <div className="App">
-    <SpeedInsights />
       <Homepage />
       <About />
       <Services />
